00ce8f72f6d8beb25310e191ad61969074ac3a5b
TanvirSourov <sourov00@live.com>
2017-05-13 08:29:57 +0600

    View and Data change

--- app/src/main/java/com/technolive/droidconbigdata/AnalyticsActivity.java
+++ app/src/main/java/com/technolive/droidconbigdata/AnalyticsActivity.java
@@ -3,6 +3,7 @@ package com.technolive.droidconbigdata;
 import android.app.ProgressDialog;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
 import android.preference.PreferenceActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -21,6 +22,10 @@ import org.eazegraph.lib.models.PieModel;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Random;
+
 import cz.msebera.android.httpclient.Header;
 
 /**
@@ -30,6 +35,8 @@ import cz.msebera.android.httpclient.Header;
 public class AnalyticsActivity extends AppCompatActivity {
 
     String my_access_token = "";
+    ArrayList<JSONObject> page_likes;
+    ArrayList<String> fb_pages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,115 +47,77 @@ public class AnalyticsActivity extends AppCompatActivity {
 
         my_access_token = getIntent().getStringExtra("my_access_token");
 
+
         final EditText first_page = (EditText) findViewById(R.id.first_page);
         final EditText second_page = (EditText) findViewById(R.id.second_page);
         final EditText third_page = (EditText) findViewById(R.id.third_page);
 
-        final PieChart mPieChart = (PieChart) findViewById(R.id.piechart);
         Button generate_analysis = (Button) findViewById(R.id.generate_analysis);
 
         generate_analysis.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String first = first_page.getText().toString();
-                String second = second_page.getText().toString();
-                String third = third_page.getText().toString();
+                fb_pages = new ArrayList<>();
+                fb_pages.add(first_page.getText().toString());
+                fb_pages.add(second_page.getText().toString());
+                fb_pages.add(third_page.getText().toString());
 
-                final String[] first_like = {""};
-                final String[] second_like = {""};
-                final String[] third_like = {""};
+                page_likes = new ArrayList<>();
 
                 AsyncHttpClient client = new AsyncHttpClient();
-                client.get("https://graph.facebook.com/" + first + "?fields=fan_count&access_token=" + my_access_token, new AsyncHttpResponseHandler() {
 
-                    @Override
-                    public void onStart() {
-                        // called before request is started
+                for (int i = 0; i < fb_pages.size(); i++) {
+                    final int finalI = i;
+                    if (finalI == 0) {
+                        page_likes.clear();
                     }
+                    client.get("https://graph.facebook.com/" + fb_pages.get(i) + "?fields=fan_count&access_token=" + my_access_token, new AsyncHttpResponseHandler() {
+                        @Override
+                        public void onStart() {
+                            // called before request is started
+                        }
+                        @Override
+                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                            try {
+                                page_likes.add(new JSONObject(new String(responseBody)));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
 
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                        first_like[0] = new String(responseBody);
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                    }
-
-                    @Override
-                    public void onRetry(int retryNo) {
-                        // called when request is retried
-                    }
-                });
-
-
-                client.get("https://graph.facebook.com/" + second + "?fields=fan_count&access_token=" + my_access_token, new AsyncHttpResponseHandler() {
-
-                    @Override
-                    public void onStart() {
-                        // called before request is started
-                    }
-
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                        second_like[0] = new String(responseBody);
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                    }
-
-                    @Override
-                    public void onRetry(int retryNo) {
-                        // called when request is retried
-                    }
-                });
-
-
-                client.get("https://graph.facebook.com/" + third + "?fields=fan_count&access_token=" + my_access_token, new AsyncHttpResponseHandler() {
-
-                    @Override
-                    public void onStart() {
-                        // called before request is started
-                    }
-
-                    @Override
-                    public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                        third_like[0] = new String(responseBody);
-
-                        int final1 = 0, final2 = 0, final3 = 0;
-                        try {
-                            JSONObject data1 = new JSONObject(first_like[0]);
-                            JSONObject data2 = new JSONObject(second_like[0]);
-                            JSONObject data3 = new JSONObject(third_like[0]);
-
-                            final1 = Integer.parseInt(data1.getString("fan_count"));
-                            final2 = Integer.parseInt(data2.getString("fan_count"));
-                            final3 = Integer.parseInt(data3.getString("fan_count"));
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+                            final Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    if (finalI == 2) {
+                                        PieChart mPieChart = (PieChart) findViewById(R.id.piechart);
+                                        mPieChart.clearChart();
+                                        Random rnd = new Random();
+                                        for (int j = 0; j < fb_pages.size(); j++) {
+                                            try {
+                                                mPieChart.addPieSlice(new PieModel("Page: " + (j + 1), Integer.parseInt(page_likes.get(j).getString("fan_count")), Color.parseColor("#" + (rnd.nextInt(999999 - 100001) + 100001))));
+                                            } catch (JSONException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                        mPieChart.startAnimation();
+                                        fb_pages.clear();
+                                    }
+                                }
+                            }, 3000);
                         }
 
+                        @Override
+                        public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                            // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                        }
 
-                        mPieChart.addPieSlice(new PieModel("First Page", final1, Color.parseColor("#FE6DA8")));
-                        mPieChart.addPieSlice(new PieModel("Second Page", final2, Color.parseColor("#56B7F1")));
-                        mPieChart.addPieSlice(new PieModel("Third Page", final3, Color.parseColor("#CDA67F")));
+                        @Override
+                        public void onRetry(int retryNo) {
+                            // called when request is retried
+                        }
+                    });
+                }
 
-                        mPieChart.startAnimation();
-                    }
-
-                    @Override
-                    public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                        // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-                    }
-
-                    @Override
-                    public void onRetry(int retryNo) {
-                        // called when request is retried
-                    }
-                });
             }
         });
 
